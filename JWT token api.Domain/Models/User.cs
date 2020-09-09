b1eb839cf3a6bc3d77005b790140ed3540fa2a0a
Ruslan <Ruslan@DESKTOP-RH0GUGH>
2020-09-09 12:36:35 +0400

    first commit

---
+++ JWT token api.Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JWT_token_api.Domain.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string UserRole { get; set; }
+    }
+}
